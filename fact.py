c4958a519ba25995e53707d53bd5bddc1d949776
lcolli111 <leenacolins4@yahoo.com>
2020-10-11 07:16:18 -0400

    Add num

---
+++ fact.py
@@ -0,0 +1,34 @@
+def factorial(n):
+      if n <= 0:
+            return 0
+      elif n == 1:
+            return 1
+      else:
+            fact = 1
+            while(n > 1):
+                  fact *= n
+                  n -= 1
+            return fact
+
+num = input("Please input a number: ")
+print("You chose",num)
+print("Factorial of", num, "is", factorial(num)) 
+print(factorial(num))
+
+#add two to the factorial
+#print sum
+
+#num = 3
+#print("Factorial of", num, "is", factorial(num))
+
+
+
+
+
+ 
+
+
+
+
+
+ 
