c812067cc1ca846b8ee418e3e09a82ac8686244c
lcolli111 <leenacolins4@yahoo.com>
2020-10-26 04:50:54 -0400

    Clean code

--- fact.py
+++ fact.py
@@ -9,16 +9,21 @@ def factorial(n):
                   fact *= n
                   n -= 1
             return fact
-
+           
 num = input("Please input a number: ")
-print("You chose",num)
-print("Factorial of", num, "is", factorial(num)) 
+#print("You chose",num)
+#print("Factorial of", num, "is", factorial(num)) 
+#for factorial in range(5):
+      #print(factorial(num))
 print(factorial(num))
 
 #Simple calculator for factorials
 
 #num = 2
 #print("Factorial of", num, "is", factorial(num))
+#for num in range(11):
+    #print(num * 2)
+
 
 
 
