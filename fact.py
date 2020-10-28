5618b0c71334ea3ce8446b6f53cc3a5d00cd8592
lcolli111 <leenacolins4@yahoo.com>
2020-10-28 02:36:59 -0400

    format.num

--- fact.py
+++ fact.py
@@ -3,8 +3,10 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-n=int(input("Input a number to compute the factiorial : "))
-print(factorial(n))
+
+num=int(input("Input a number to compute the factiorial : "))
+print("The factorial is: ".format(num))
+print(factorial(num))
 
 
 
