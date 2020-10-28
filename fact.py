33b4118fd9123dd299cf5bf1f277f9c5a12cb6e4
lcolli111 <leenacolins4@yahoo.com>
2020-10-28 02:22:56 -0400

    Too many mistakes!

--- fact.py
+++ fact.py
@@ -1,21 +1,55 @@
 def factorial(n):
-      if n <= 0:
-            return 0
-      elif n == 1:
-            return 1
-      else:
-            fact = 1
-            while(n > 1):
-                  fact *= n
-                  n -= 1
-            return fact
-           
-num = input("Please input a number: ")
-#print("You chose",num)
-#print("Factorial of", num, "is", factorial(num)) 
-#for factorial in range(5):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
+
+
+
+
+#def factorial(n):
+      #if n <= 0:
+            #return 0
+      #elif n == 1:
+            #return 1
+      #else:
+            #fact = 1
+            #while(n > 1):
+                  #fact *= n
+                  #n -= 1
+            #return fact
+
+#def getNumber():
+     # num = int(input("Input a number under 11: "))
+      #if (num<= 10):
+            #return num
+      #else:
+            #print("Sorry, the number is over 11. Please try again!")
+            #return = -1
+
+      #while True:
+            #result != -1
+            #while(result = 0):
+                  #result = 
+      #getNumber()
+
+           # fact = factorial(result)
+            #print("Factorial of", result, "is", fact)
+
+            #again = input("Do you want to input another number? (y/n)").lower()
+            #if again != "y":
+                  #break
+
+         
+
+
+#print(factorial(num))
+#How can I use a range to limit input numbers from 1 to 10?
+
+#for num in range(5):
       #print(factorial(num))
-print(factorial(num))
 
 #Simple calculator for factorials
 
