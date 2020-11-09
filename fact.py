3893ef577a7105e136be4b4f67e9c8187d973a4c
lcolli111 <leenacolins4@yahoo.com>
2020-11-09 05:45:35 -0500

    Need Value Error

--- fact.py
+++ fact.py
@@ -1,19 +1,23 @@
 def factorial(n):
+      while True:
 
-    if n == 0:
-        return 1
+            if n == 0:
+                  return 1
         
         
-    elif n <= 0:
+            elif n <= 0:
           
-          print("Too negative!")
-          ans = int(input("Try again: "))
-    elif n >= 10:
-          print("Too loud! Keep it down!")
-          ans = int(input("Try another number: ")) #Need to include another loop for three tries
-    else:
-        return n * factorial(n-1) #Recursive function used to compute the number
-        
+                  print("Too negative!")
+                  ans = int(input("Try again: "))
+            elif n >= 10:
+                
+                  print("Too loud! Keep it down!")
+                  ans = int(input("Try another number: "))
+            
+                   #Need to include another loop for three tries
+            else:
+                  return n * factorial(n-1) #Recursive function used to compute the number
+                  break
 n=int(input("Input a number under eleven : "))
 print("The factorial of", n, "is:", factorial(n))
 
@@ -28,29 +32,3 @@ def num():
 
 num()
 
-#def ask_again():
-      #ans = str(input("Do you want to put another number in y/n?  "))
-      
-      #if ans == 'y':
-            #return num
-
-      #else:
-
-            #quit
-#ask_again()   
-# ('y', 'n') 
-      
-            
-      #else:
-            #if ans == 'n':
-
-                  #print ("Good bye!")
-                  #quit
-
-                
-#while True:
-      #result = -1
-      #while (result < 0):
-            #result = getNumber()
-      #fact = factorial(result)
-      #print("Factorial of ", result, "is", fact)
